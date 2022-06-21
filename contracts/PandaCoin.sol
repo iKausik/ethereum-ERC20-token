2ea7bbd253c7c7ba4b56dfa489e971bd146e777c
Kausik Das <kausik007007@gmail.com>
2022-06-21 23:01:36 +0530

    deployer script added

--- contracts/PandaCoin.sol
+++ contracts/PandaCoin.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract PandaCoin is ERC20, Ownable, ERC20Buranable {
+contract PandaCoin is ERC20, Ownable, ERC20Burnable {
     event tokenMinted(address indexed owner, uint256 amount, string message);
     event additionalTokenMinted(
         address indexed owner,
